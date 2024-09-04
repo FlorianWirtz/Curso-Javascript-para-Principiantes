431280e5a3cb897c61c1dc7bb404cc6952746591
FlorianWirtz <jaimesebas3004gmail.com>
2024-09-03 22:11:02 -0500

    Se agrega el proyecto

---
+++ javascrip.js
@@ -0,0 +1,122 @@
+// consola
+//document.write("<h1>Hola Mundo</h1>");
+
+/*//tipos de datos
+"hola mundo"//string
+'Hola Mundo'//stirng
+
+
+
+1000000
+-2.3
+
+// boolean
+true
+false
+
+// array
+['joe', 'ryan', 'martha']
+[1, 2, 3]
+[true, false, true, false]
+
+// object
+'ryan'
+70.4
+14
+true
+
+{
+ "username": 'ryan',
+ "score": 70.4,
+ "hours": 14,
+ "proffesional": true
+}*/
+
+// VARIABLES
+/*// estoy crenado variable
+
+var nombredelusuario = "john";
+let lastname = "carter";
+
+// reasignando el valor de la variable nombredelusuario
+
+nombredelusuario = 'pepe';
+
+const PI = 3.1415;
+
+
+
+console.log(PI)
+
+// camelcase
+
+let nombreDePersona = 'gordon';
+
+*/
+// OPERADORES
+/*let passwordDB = 'pepe123'
+
+let input = 'pepe123'
+
+let result = input == passwordDB;
+
+if (result == true) {
+    console.log('login correcto');
+}
+
+if(result == false){
+    console.log('login Incorrecto');
+} else {
+    console.log('contraseña incorrecta')
+}*/
+
+/*let score = 70;
+
+if (score > 30){
+    console.log('Necesitas practicar más');
+}  else if (score > 15) {
+    console.log('Estas mejorando')
+}
+
+else {
+        console.log('Necesitas seguir este tutorial')
+}*/
+// condicionales
+/*let typeCard = 'Credit Card';
+
+switch(typeCard) {
+    case 'Debid Card':
+        console.log('This is a debid card');
+        break;
+   case 'Credit Card':
+      console.log('This is a credit card');
+      break;
+    default:
+      console.log('No Card');
+}*/
+// BUCLES
+/*let count = 0;
+
+
+while(count < 50) {
+    console.log(count);
+    count++;
+}*/
+
+/*let names = ['ryan','joe','john'];
+
+for(let i= names.length - 1; i >= 0; i--) {
+  console.log(names[i]);
+}*/
+// funcion
+function add(n1,n2) {
+    console.log(n1 + n2);
+}
+
+add(3,2);
+add(100,300);
+add(3123123123, 132123123);
+
+
+
+
